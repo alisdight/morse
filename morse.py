969ea48017b873deaf5cb92d4812e6e19c4a29ae
alisdight <82391709+alisdight@users.noreply.github.com>
2022-01-17 17:42:41 +0400

    Update morse.py

--- morse.py
+++ morse.py
@@ -38,16 +38,19 @@ def decode_from_morse(code):
 
 def main():
     while True:
-        print("Вы хотите закодировать (0) или раскодировать(1) текст?")
+        print("Вы хотите закодировать (1) или раскодировать(2) текст? 0 - выйти.")
         user_operation = input()
-        if user_operation == "0":
+        if user_operation == "1":
             print("Введите текст:")
             text = input().upper()
             print(encode_to_morse(text))
-        elif user_operation == "1":
+        elif user_operation == "2":
             print("Введите код:")
             code = input()
             print(decode_from_morse(code))
+        elif user_operation == "0":
+            print("Всего доброго.")
+            break
 
 
 if __name__ == '__main__':
